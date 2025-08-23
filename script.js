c66ba6345baea88c093b4691e442aae0bc153c97
kuldeep2300 <kv5228920@gmail.com>
2025-08-23 18:50:06 +0530

    Dynamically rendering the content using JS

--- script.js
+++ script.js
@@ -1,13 +1,128 @@
-let API_URL = "https://dummyjson.com/products?limit=15";
+const products = document.getElementById("products");
+const priceLowHigh = document.getElementById("sortPriceLowHigh");
+const priceHighLow = document.getElementById("sortPriceHighLow");
+const ratingHighLow = document.getElementById("sortRating");
 
+let allProducts = [];
+
+//? Rendering cards in product-container
+const renderCards = (array) => {
+  products.innerHTML = "";
+  array.forEach((product) => {
+    let cardElem = document.createElement("div");
+    let discountPrice =
+      product.price - (product.price / 100) * product.discountPercentage;
+
+    cardElem.innerHTML = `
+      <div class="card">
+            <div class="imgContainer">
+                <img class="mainImage" src="${
+                  product.thumbnail
+                }" alt="product-image">
+            </div>
+            <h2 class="title">${product.title}</h2>
+            <p class="price">Rs. ${product.price}</p>
+            <p class="discountPrice">Rs. ${discountPrice.toFixed(2)}</p>
+            <p class="discountPercentage">save ${
+              product.discountPercentage
+            }%</p>
+            <p class="rating"></p>
+
+            <div class="btnContainer">
+                <button class="showDescBtn">Show Description</button>
+                <button class="addToCartBtn">Add to cart</button>
+            </div>
+            <div class="description" style="display:none;">${
+              product.description
+            }</div>
+         </div>
+   `;
+
+    let showDescBtn = cardElem.querySelector(".showDescBtn");
+    let descriptionDiv = cardElem.querySelector(".description");
+    showDescBtn.addEventListener("click", () => {
+      if (descriptionDiv.style.display === "none") {
+        descriptionDiv.style.display = "block";
+        showDescBtn.textContent = "Less Description";
+        showDescBtn.style.cssText = "color: white; background-color: #001442";
+      } else {
+        descriptionDiv.style.display = "none";
+        showDescBtn.textContent = "Show Description";
+        showDescBtn.style.cssText = "";
+      }
+    });
+
+    products.append(cardElem);
+  });
+};
+
+//? Printing star
+const handlePrintingRating = () => {
+
+}
+
+//? Fetching API Data
 const fetchAPI = async () => {
+  let API_URL = "https://dummyjson.com/products?limit=15";
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    console.log(data.products);
+
+    allProducts = data.products;
+    renderCards(allProducts);
   } catch (error) {
     console.error(error);
   }
 };
+
+//? Sorting price low to high button
+const handleSortPriceLowHigh = () => {
+  let priceAscArray = JSON.parse(JSON.stringify(allProducts));
+  priceAscArray = priceAscArray.sort((a, b) => a.price - b.price);
+  // Re-render sorting arr
+  renderCards(priceAscArray);
+
+  // Highlight only active button
+  [priceLowHigh, priceHighLow, ratingHighLow].forEach((btn) => {
+    btn.style.cssText = `background-color:''; color: '';`;
+  });
+
+  priceLowHigh.style.cssText = "background-color: blue; color: white;";
+};
+
+//? Sorting price high to low button
+const handleSortPriceHighLow = () => {
+  let priceDesArray = JSON.parse(JSON.stringify(allProducts));
+  priceDesArray = priceDesArray.sort((a, b) => b.price - a.price);
+  // Re-render sorting arr
+  renderCards(priceDesArray);
+
+  // Highlight only active button
+  [priceLowHigh, priceHighLow, ratingHighLow].forEach((btn) => {
+    btn.style.cssText = `background-color:''; color: '';`;
+  });
+
+  priceHighLow.style.cssText = "background-color: blue; color: white;";
+};
+
+//? Sorting rating high to low button
+const handleSortRating = () => {
+  let ratingAcsArray = JSON.parse(JSON.stringify(allProducts));
+  ratingAcsArray = ratingAcsArray.sort((a, b) => a.rating - b.rating);
+  // Re-render sorting arr
+  renderCards(ratingAcsArray);
+
+  // Highlight only active button
+  [priceLowHigh, priceHighLow, ratingHighLow].forEach((btn) => {
+    btn.style.cssText = `background-color:''; color: '';`;
+  });
+
+  ratingHighLow.style.cssText = "background-color: blue; color: white;";
+};
+
+//? Event Listeners on the buttons
+priceLowHigh.addEventListener("click", handleSortPriceLowHigh);
+priceHighLow.addEventListener("click", handleSortPriceHighLow);
+ratingHighLow.addEventListener("click", handleSortRating);
 
 fetchAPI();
