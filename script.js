7f3cf6f2fc0f91938c2afcb641995e7deb68903e
kuldeep2300 <kv5228920@gmail.com>
2025-08-23 10:08:11 +0530

    First Fetching data from an - API & Console it for debugging

---
+++ script.js
@@ -0,0 +1,13 @@
+let API_URL = "https://dummyjson.com/products?limit=15";
+
+const fetchAPI = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchAPI();
